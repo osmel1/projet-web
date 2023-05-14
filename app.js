e03e648332dc0598a356cf886dc34ad05513ee74
osmel1 <110778429+osmel1@users.noreply.github.com>
2023-05-14 11:42:30 +0100

    data base done

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 const app = express()
-
+const test= require('./routes/test')
 app.get('/', async (req, res) => {
-  const result = await prisma.utilisateur.count()
+  const result = await prisma.user.count()
   res.json(result)
 })
-
+app.use('/test',test);
 app.listen(3000, () => {
   console.log('Server started on http://localhost:3000')
 })
